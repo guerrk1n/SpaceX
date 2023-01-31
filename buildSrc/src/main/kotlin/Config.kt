2d9f518b8d72a937982f7cba77d582483c9c01d4
A.Skubilov <aleksej684549@inbox.ru>
2023-01-31 21:47:29 +0300

    Update tagret and compile SDK version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val minSdk = 27
-    const val targetSdk = 32
-    const val compileSdk = 32
+    const val targetSdk = 33
+    const val compileSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
     val javaVersion = JavaVersion.VERSION_11
