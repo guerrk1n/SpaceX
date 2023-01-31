7ddfbcab1594d8fd03e5579f57e22dbbb7383a93
A.Skubilov <aleksej684549@inbox.ru>
2023-01-31 21:42:09 +0300

    Update libs versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,16 +1,16 @@
 object Dependencies {
 
-    const val ktlint = "com.pinterest:ktlint:0.46.1"
-    const val coil = "io.coil-kt:coil-compose:2.1.0"
-    const val gson = "com.google.code.gson:gson:2.9.1"
+    const val ktlint = "com.pinterest:ktlint:0.48.2"
+    const val coil = "io.coil-kt:coil-compose:2.2.2"
+    const val gson = "com.google.code.gson:gson:2.10.1"
     const val timber = "com.jakewharton.timber:timber:5.0.1"
 
     object Kotlin {
-        const val version = "1.7.0"
+        const val version = "1.8.0"
     }
 
     object Hilt {
-        const val version = "2.43.2"
+        const val version = "2.44.2"
         const val hilt = "com.google.dagger:hilt-android:$version"
         const val plugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
         const val compiler = "com.google.dagger:hilt-compiler:$version"
@@ -28,12 +28,12 @@ object Dependencies {
 
         object VersionsPlugin {
             const val id = "com.github.ben-manes.versions"
-            const val version = "0.42.0"
+            const val version = "0.44.0"
         }
     }
 
     object Room {
-        const val version = "2.4.3"
+        const val version = "2.5.0"
         const val ktx = "androidx.room:room-ktx:$version"
         const val runtime = "androidx.room:room-runtime:$version"
         const val kapt = "androidx.room:room-compiler:$version"
@@ -47,21 +47,21 @@ object Dependencies {
     }
 
     object Compose {
-        const val version = "1.2.0"
-        const val compiler = "1.2.0"
+        const val version = "1.3.1"
+        const val compiler = "1.4.0"
         const val ui = "androidx.compose.ui:ui:$version"
         const val material = "androidx.compose.material:material:$version"
         const val tooling = "androidx.compose.ui:ui-tooling:$version"
         const val toolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
-        const val paging = "androidx.paging:paging-compose:1.0.0-alpha15"
-        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-rc02"
-        const val activity = "androidx.activity:activity-compose:1.5.0"
+        const val paging = "androidx.paging:paging-compose:1.0.0-alpha17"
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1"
+        const val activity = "androidx.activity:activity-compose:1.6.1"
         const val runtime = "androidx.compose.runtime:runtime:$version"
         const val util = "androidx.compose.ui:ui-util:$version"
-        const val navigation = "androidx.navigation:navigation-compose:2.5.1"
+        const val navigation = "androidx.navigation:navigation-compose:2.5.3"
 
         object Accompanist {
-            private const val version = "0.25.0"
+            private const val version = "0.28.0"
 
             const val pager = "com.google.accompanist:accompanist-pager-indicators:$version"
             const val pagerIndicator = "com.google.accompanist:accompanist-pager:$version"
@@ -77,7 +77,7 @@ object Dependencies {
     }
 
     object Pluto {
-        const val version = "2.0.4"
+        const val version = "2.0.9"
 
         object Debug {
             const val pluto = "com.plutolib:pluto:$version"
